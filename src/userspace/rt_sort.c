e24365e7490285f913be8d491ad4728759d5b8d1
junk985 <kagantemur123@gmail.com>
2025-11-07 14:16:51 +0300

    implement merge sort functionality with global buffer in rt_sort.c

--- src/userspace/rt_sort.c
+++ src/userspace/rt_sort.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 
 #define PERIOD_NS 10000000L  // 10 ms
+#define SIZE 50
+
 
 static volatile sig_atomic_t running = 1;
 
@@ -17,6 +19,48 @@ void handle_sigint(int sig) {
     (void)sig;
     running = 0;
 }
+
+
+
+static int merge_buffer[SIZE]; // global buffer
+
+void merge(int* array, size_t start, size_t middle, size_t end) {
+    size_t i, j, k;
+
+    i = start;
+    j = middle + 1;
+    k = start;
+
+    while (i <= middle && j <= end) {
+        if (array[i] <= array[j]) {
+            merge_buffer[k++] = array[i];
+        } else {
+            merge_buffer[k++] = array[j];
+        }
+    }
+    
+    while (i <= middle) { 
+        merge_buffer[k++] = array[i++]; 
+    }
+
+    while (j <= end) { 
+        merge_buffer[k++] = array[j++]; 
+    }
+
+    for (int i = start; i < end; i++) {
+        array[i] = merge_buffer[i];
+    }
+
+}
+
+void merge_sort(int* array, size_t start, size_t end) {
+    if (start < end) {
+        size_t middle = (start + end) / 2;
+        merge_sort(array, start, middle);
+        merge_sort(array, middle+1, end);
+        merge(array, start, middle, end);
+    }
+} 
 
 static unsigned int x = 123456789, y = 362436069, z = 521288629;
 static inline unsigned int xor_random(void) {
